d64ff117f90a0d45a5fa9bd0a3936348c3277010
topdev4876 <xldev007@gmail.com>
2022-08-25 23:49:26 +0900

    random function for rarity

---
+++ src/components/rarity.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function getRandom (weights) {
+    // weights = [0.3, 0.3, 0.3, 0.1]
+    var num = Math.random(),
+        s = 0,
+        lastIndex = weights.length - 1;
+
+    for (var i = 0; i < lastIndex; ++i) {
+        s += weights[i];
+        if (num < s) {
+            return i + 1;
+        }
+    }
+
+    return lastIndex + 1;
+};
+
+
