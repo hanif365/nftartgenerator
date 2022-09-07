b0a3948672ef292f7063a3a7e7611b5048b51c15
hanif365 <m.a.hanifkhan64@gmail.com>
2022-09-08 02:05:36 +0600

    traits image added in rarity modal

--- src/components/Multislider/index.js
+++ src/components/Multislider/index.js
@@ -2,38 +2,40 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import { useState, useEffect } from "react"
-import {Container,Row,Col} from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
 import Switch from '@mui/material/Switch';
+import './index.css';
 
-function MultiSlider({Items, sendValues, values, layername}){
+function MultiSlider({ Items, sendValues, values, layername }) {
+    console.log("Items : ", Items);
 
     const [advancedMode, setAdvancedMode] = useState(false)
     const [advancedvalues, setAdvancedValues] = useState([])
     const [totalRange, setTotalRange] = useState()
 
-    useEffect(()=>{
+    useEffect(() => {
         setAdvancedValues(values[layername])
         setTotalRange(values[layername].reduce((x, y) => parseInt(x) + parseInt(y)))
-    },[])
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
 
         let arr = []
-        advancedvalues.forEach((item, index)=>{
+        advancedvalues.forEach((item, index) => {
             arr.push((item / totalRange * 100).toFixed(2))
         })
         let obj = {}
         obj[layername] = arr
         sendValues(layername, obj)
 
-    },[advancedvalues])
+    }, [advancedvalues])
 
-      function advanceHandleChange(index, value) {
+    function advanceHandleChange(index, value) {
         let arr = [...advancedvalues]
         arr[index] = value
         setAdvancedValues(arr)
         setTotalRange(arr.reduce((x, y) => parseInt(x) + parseInt(y)))
-      }
+    }
 
     return (
         <Box>
@@ -43,65 +45,69 @@ function MultiSlider({Items, sendValues, values, layername}){
                 </Col>
 
                 <Col xs={12} md={3}>
-                    <Switch checked = {advancedMode} defaultChecked onChange={ e => setAdvancedMode(e.target.checked)}/>
+                    <Switch checked={advancedMode} defaultChecked onChange={e => setAdvancedMode(e.target.checked)} />
                 </Col>
 
             </Row>
-            
-            { Items ?
-                Items.map((item, index)=>
-                    <div key={index} className = "margin">
+
+            {Items ?
+                Items.map((item, index) =>
+                    // console.log("Item  : ",item)
+
+                    <div key={index} className="margin">
                         {
-                            !advancedMode?
-                            <Row>
-                                <Col xs={12} md={6}>
-                                    <Row>
-                                        <Col xs={6} md={6}>
-                                            {item.file.name}
-                                        </Col>
-                                        <Col xs={6} md={6}>
-                                            { (advancedvalues[index] / totalRange * 100).toFixed(2)}%
-                                        </Col>
-                                    </Row>
-                                </Col>
-                                <Col xs={12} md={6}>
-                                    <Slider
-                                        size="small"
-                                        value={advancedvalues[index] / totalRange * 100}
-                                        aria-label="Small"
-                                        valueLabelDisplay="auto"
-                                        onChange={(e) => advanceHandleChange(index, e.target.value)}
-                                    />
-                                </Col>
-                            </Row>:
-                            <Row>
-                                <Col xs={12} md={6}>
-                                    <Row>
-                                        <Col xs={6} md={6}>
-                                            {item.file.name}
-                                        </Col>
-                                        <Col xs={6} md={6}>
-                                            { (advancedvalues[index] / totalRange * 100).toFixed(2)}%
-                                        </Col>
-                                    </Row>
-                                </Col>
-                                <Col xs={12} md={6}>
-                                    <input
-                                        type="number"
-                                        value={advancedvalues[index]}
-                                        onChange={(e) => advanceHandleChange(index, e.target.value)}
-                                    /> out of {totalRange}
-                                </Col>
-                            </Row>
+                            !advancedMode ?
+                                <Row className='d-flex'>
+                                    <Col xs={12} md={6} className="align-self-center">
+                                        <Row>
+                                            <Col xs={6} md={6}>
+                                                {/* {item.file.name} */}
+                                                <img src={item['data_url']} className="traits_img" alt="traits_img" />
+                                            </Col>
+                                            <Col xs={6} md={6} className="mt-4">
+                                                {(advancedvalues[index] / totalRange * 100).toFixed(2)}%
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col xs={12} md={6} className="align-self-center mt-4">
+                                        <Slider
+                                            size="small"
+                                            value={advancedvalues[index] / totalRange * 100}
+                                            aria-label="Small"
+                                            valueLabelDisplay="auto"
+                                            onChange={(e) => advanceHandleChange(index, e.target.value)}
+                                        />
+                                    </Col>
+                                </Row> :
+                                <Row className='d-flex justify-content-center'>
+                                    <Col xs={12} md={6} className="align-self-center">
+                                        <Row>
+                                            <Col xs={6} md={6}>
+                                                {/* {item.file.name} */}
+                                                <img src={item['data_url']} className="traits_img" alt="traits_img" />
+                                            </Col>
+                                            <Col xs={6} md={6} className="mt-4">
+                                                {(advancedvalues[index] / totalRange * 100).toFixed(2)}%
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col xs={12} md={6} className="align-self-center mt-4">
+                                        <input
+                                            type="number"
+                                            value={advancedvalues[index]}
+                                            onChange={(e) => advanceHandleChange(index, e.target.value)}
+                                        /> out of {totalRange}
+                                    </Col>
+                                </Row>
                         }
-                        
+
                     </div>
                 )
                 :
                 <></>
             }
         </Box>
-      );
+    );
 }
 
 export default MultiSlider;
